df8a798da51f8ac2c1624ad1a7472aadddfd33c7
Sayan Sadhukhan <sayansadhukhan62@gmail.com>
2020-06-24 23:48:52 +0530

    fff

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="shortcut icon" href="{{asset('/storage/favicon.ico')}}">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
