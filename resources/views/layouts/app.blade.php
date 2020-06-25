94e96aea6ef7ad89e91705776338d37b5e71c48a
Sayan Sadhukhan <sayansadhukhan62@gmail.com>
2020-06-26 02:27:52 +0530

    d

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,11 +63,11 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="username nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="username nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
